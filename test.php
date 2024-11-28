60cc1e4dfef35014d289b3ea2e3a98fbe45ffb89
Refisaa <refisarahayu@gmail.com>
2024-11-28 13:28:41 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+function tampilanNama(){
+    echo "hello nama saya Refi sabila rahayu";
+}
+tampilanNama();
+
+function penjumlahan($nilai2,$nilai3){
+$hasil = $nilai2 + $nilai2;
+return $hasil;
+}
+echo(penjumlahan(5234,987));
+
